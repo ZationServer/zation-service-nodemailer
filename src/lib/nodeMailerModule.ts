399c8ca1b13d417015a8ea19f09760463ba83136
Luca Scaringella <lucalstyle@gmail.com>
2019-05-04 20:05:41 +0200

    update nodemailer service with new zation-service interface

--- src/lib/nodeMailerModule.ts
+++ src/lib/nodeMailerModule.ts
@@ -5,15 +5,15 @@ GitHub: LucaCode
  */
 
 import * as SMTPTransport from "nodemailer/lib/smtp-transport";
-import * as SMTPPool from "nodemailer/lib/smtp-pool";
+import * as SMTPPool      from "nodemailer/lib/smtp-pool";
 import * as SendmailTransport from "nodemailer/lib/sendmail-transport";
 import * as StreamTransport from "nodemailer/lib/stream-transport";
-import * as JSONTransport from "nodemailer/lib/json-transport";
-import * as SESTransport from "nodemailer/lib/ses-transport";
+import * as JSONTransport   from "nodemailer/lib/json-transport";
+import * as SESTransport    from "nodemailer/lib/ses-transport";
 import {Transport, TransportOptions, Transporter, createTransport, SentMessageInfo} from "nodemailer";
-import {ServiceModule} from "zation-service";
-import {SmallBag, Bag} from "zation-server";
-import {Options} from "nodemailer/lib/mailer";
+import {ServiceModule}   from "zation-service";
+import {SmallBag}        from "zation-server";
+import {Options}         from "nodemailer/lib/mailer";
 
 const serviceName = "NodeMailer";
 
@@ -29,7 +29,7 @@ export type NodeMailerConfig = (
 
 export namespace NodeMailerModule {
 
-    export function build(configs: Record<string, NodeMailerConfig> | DefaultConfig<NodeMailerConfig>): ServiceModule<NodeMailerConfig, Transporter, {}> {
+    export function build(configs: Record<string, NodeMailerConfig> | DefaultConfig<NodeMailerConfig>): ServiceModule<NodeMailerConfig, Transporter, {},SmallBagExtension> {
         const service: any = configs;
         service.get = undefined;
         service.create = async (c): Promise<Transporter> => {
@@ -51,23 +51,22 @@ export namespace NodeMailerModule {
             service: service,
             bagExtensions: {
                 smallBag: {
-                    sendMail: async function(this: SmallBag,mailOptions : Options,serviceKey : string = 'default') : Promise<SentMessageInfo> {
+                    sendMail: async function(this: SmallBag,mailOptions : Options,configName : string = 'default') : Promise<SentMessageInfo> {
                         return new Promise<SentMessageInfo>(async (resolve, reject) => {
-                            const service = await this.getService<Transporter>(serviceName, serviceKey);
+                            const service = await this.getService<Transporter>(serviceName, configName);
                             service.sendMail(mailOptions,(error, info) => {
                                 if(error) {reject(error);}
                                 else {resolve(info);}
                             });
                         });
                     },
-                    getNodeMailer: async function (this: SmallBag, serviceKey: string = 'default'): Promise<Transporter> {
-                        return await this.getService<Transporter>(serviceName, serviceKey);
+                    getNodeMailer: async function (this: SmallBag, configName: string = 'default'): Promise<Transporter> {
+                        return await this.getService<Transporter>(serviceName, configName);
                     },
-                    isNodeMailer: function (this: SmallBag, serviceKey: string = 'default'): boolean {
-                        return this.isService(serviceName, serviceKey);
+                    isNodeMailer: function (this: SmallBag, configName: string = 'default'): boolean {
+                        return this.isService(serviceName, configName);
                     }
-                },
-                bag: {}
+                }
             }
         };
     }
@@ -78,87 +77,46 @@ interface DefaultConfig<T> {
     default?: T;
 }
 
-declare module 'zation-server' {
-    export interface SmallBag {
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Send an email, with the node mailer service.
-         * Throws an ServiceNotFoundError if the service is not found.
-         * @example
-         * let mailOptions = {
-         *  from: '"Fred Foo 👻" <foo@example.com>', // sender address
-         *  to: 'bar@example.com, baz@example.com', // list of receivers
-         *  subject: 'Hello ✔', // Subject line
-         *  text: 'Hello world?', // plain text body
-         *  html: '<b>Hello world?</b>' // html body
-         *  };
-         * const info = await sendMail(mailOptions);
-         * @throws ServiceNotFoundError
-         * @param mailOptions
-         * @param  serviceKey
-         * @return Promise<object>
-         * The object is an info object.
-         */
-        sendMail: (mailOptions: Options, serviceKey ?: string) => Promise<SentMessageInfo>;
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Returns this service, if it exist otherwise it will throw an ServiceNotFoundError error.
-         * @throws ServiceNotFoundError
-         * @param  serviceKey
-         * the key to the service.
-         */
-        getNodeMailer: (serviceKey ?: string) => Promise<Transporter>;
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Checks if the service with this key is exist and can be used.
-         * @param  serviceKey
-         * the key to the service.
-         */
-        isNodeMailer: (serviceKey ?: string) => boolean;
-    }
-
-    export interface Bag {
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Send an email, with the node mailer service.
-         * Throws an ServiceNotFoundError if the service is not found.
-         * @example
-         * let mailOptions = {
-         *  from: '"Fred Foo 👻" <foo@example.com>', // sender address
-         *  to: 'bar@example.com, baz@example.com', // list of receivers
-         *  subject: 'Hello ✔', // Subject line
-         *  text: 'Hello world?', // plain text body
-         *  html: '<b>Hello world?</b>' // html body
-         *  };
-         * const info = await sendMail(mailOptions);
-         * @throws ServiceNotFoundError
-         * @param mailOptions
-         * @param  serviceKey
-         * @return Promise<object>
-         * The object is an info object.
-         */
-        sendMail: (mailOptions: Options, serviceKey ?: string) => Promise<SentMessageInfo>;
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Returns this service, if it exist otherwise it will throw an ServiceNotFoundError error.
-         * @throws ServiceNotFoundError
-         * @param  serviceKey
-         * the key to the service.
-         */
-        getNodeMailer: (serviceKey ?: string) => Promise<Transporter>;
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Checks if the service with this key is exist and can be used.
-         * @param  serviceKey
-         * the key to the service.
-         */
-        isNodeMailer: (serviceKey ?: string) => boolean;
-    }
+interface SmallBagExtension {
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Send an email promise based with a node mailer service.
+     * Throws a ServiceNotFoundError if the service is not found.
+     * @example
+     * const mailOptions = {
+     *  from: '"Fred Foo 👻" <foo@example.com>', // sender address
+     *  to: 'bar@example.com, baz@example.com', // list of receivers
+     *  subject: 'Hello ✔', // Subject line
+     *  text: 'Hello world?', // plain text body
+     *  html: '<b>Hello world?</b>' // html body
+     *  };
+     * const info = await sendMail(mailOptions);
+     * @throws ServiceNotFoundError
+     * @param mailOptions
+     * @param configName Default: 'default'
+     * @return Promise<object>
+     * The object is an info object.
+     */
+    sendMail: (mailOptions: Options, configName ?: string) => Promise<SentMessageInfo>;
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Returns the node mailer service, if it exists otherwise, it will throw a ServiceNotFoundError error.
+     * @throws ServiceNotFoundError
+     * @param  configName Default: 'default'
+     */
+    getNodeMailer: (configName ?: string) => Promise<Transporter>;
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * This function returns a boolean that indicates if the node mailer service with the given configuration name exists.
+     * @param configName Default: 'default'
+     */
+    isNodeMailer: (configName ?: string) => boolean;
 }
 
+declare module 'zation-server' {
+    export interface SmallBag extends SmallBagExtension{
+    }
+}
